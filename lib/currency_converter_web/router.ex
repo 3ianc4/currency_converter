79c04d2e2f46bfc9c5b3f4684537bb9118aafaad
Bianca Guzenski <biancalway@gmail.com>
2024-06-16 17:14:14 -0300

    Add routes to transactions and users

--- lib/currency_converter_web/router.ex
+++ lib/currency_converter_web/router.ex
@@ -6,7 +6,6 @@ defmodule CurrencyConverterWeb.Router do
     plug :fetch_session
     plug :fetch_live_flash
     plug :put_root_layout, html: {CurrencyConverterWeb.Layouts, :root}
-    plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
@@ -20,18 +19,15 @@ defmodule CurrencyConverterWeb.Router do
     get "/", PageController, :home
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", CurrencyConverterWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", CurrencyConverterWeb do
+    pipe_through :browser
 
-  # Enable LiveDashboard in development
+    post "/users", UsersController, :create
+    get "/transactions", TransactionsController, :list
+    post "/convert_currencies", TransactionsController, :convert_currencies
+  end
+
   if Application.compile_env(:currency_converter, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
