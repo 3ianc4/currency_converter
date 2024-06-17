d382a85f02e89a0648621f927752f183ce091f54
Bianca Guzenski <biancalway@gmail.com>
2024-06-16 21:14:15 -0300

    Add Retry to retry request in case of timeout

--- lib/currency_converter/services/converter.ex
+++ lib/currency_converter/services/converter.ex
@@ -1,12 +1,18 @@
 defmodule CurrencyConverter.Converter do
   @moduledoc false
+  use Retry
 
   def convert(%{"to" => to, "from" => from, "amount" => amount}) do
     url = "#{config()[:base_api_url]}to=#{to}&from=#{from}&amount=#{amount}"
 
     headers = [{"apikey", config()[:api_key]}]
 
-    case HTTPoison.get(url, headers) do
+    result =
+      retry with: exponential_backoff() |> randomize |> cap(1_000) |> expiry(10_000) do
+        HTTPoison.get(url, headers)
+      end
+
+    case result do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         {:ok, Jason.decode!(body)}
 
